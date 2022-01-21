85758a57838d6f06206819414d15fe5809e8861b
AfreenKazi <AKAZI@beckman.com>
2022-01-21 11:22:27 +0530

    counter file updated with incrementing code in init method

--- counter.py
+++ counter.py
@@ -3,9 +3,6 @@ class Counter:
     count = 0
 
     def __init__(self):
-        self.count = 0
-
-    def add_one(self):
         Counter.count += 1
 
     def get_counter(self):
@@ -13,13 +10,10 @@ class Counter:
 
 
 obj1 = Counter()
-obj1.add_one()
 print(obj1.get_counter())
 obj2 = Counter()
-obj2.add_one()
 print(obj2.get_counter())
 obj3 = Counter()
-obj3.add_one()
 print(obj3.get_counter())
 obj4 = Counter()
 print(obj4.get_counter())
