66a6bb3fa505d6bad555697669e4bccd572eb1d7
AfreenKazi <AKAZI@beckman.com>
2022-01-28 18:38:52 +0530

    verify a given roman number is valid or not using regular expression

---
+++ RomanNumber.py
@@ -0,0 +1,5 @@
+# to check whether a number is roman number or not, if yes print True else False
+
+import re
+regex_pattern = r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"
+print(str(bool(re.match(regex_pattern, input()))))
